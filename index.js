8777ba8acb1137a783169667d8b117f425364b0f
August Conwell <august.conwell@gmail.com>
2018-02-26 20:23:29 -0500

    contenteditable directive exported from index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const ContentEditable = {
+  install(Vue) {
+    Vue.directive("contenteditable", {
+      bind(el, { expression }, vnode) {
+        el.contentEditable = true;
+        el.oninput = function(event) {
+          vnode.context[expression] = event.target.innerText;
+          el.dataset.comparison = event.target.innerText;
+        };
+        el.onblur = function(event) {
+          el.innerText = el.dataset.message;
+        };
+      },
+      componentUpdated: function(el, { expression }, vnode) {
+        const message = vnode.context[expression];
+        el.dataset[expression] = message;
+        if (el.dataset[expression] !== el.dataset.comparison) {
+          el.innerText = vnode.context[expression];
+        }
+      }
+    });
+  }
+};
+
+export default contenteditable;
